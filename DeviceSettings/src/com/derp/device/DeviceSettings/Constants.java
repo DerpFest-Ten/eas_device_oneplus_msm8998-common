DerpFest-Ten/bak_device_oneplus_msm8998-common

msm8998-common: DeviceSettings: Add Touchscreen Gesture controls
Imported from LineagePart, adapted to LineageHW implementation present in fwb instead of lineage-sdk

Change-Id: I08064ca6c7a7e58783847677843522b818c583de
TouchscreenGestureSettings class written by:
Co-authored-by: Anushek Prasal <anushekprasal@gmail.com>
Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>
Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

DeviceSettings/src/com/aosip/device/DeviceSettings/Constants.java
@@ -31,6 +31,9 @@ import android.media.AudioManager;
 
 public class Constants {
 
+    // Broadcast action for settings update
+    static final String UPDATE_PREFS_ACTION = "com.aosip.device.DeviceSettings.UPDATE_SETTINGS";
+
     // Preference keys
     public static final String NOTIF_SLIDER_TOP_KEY = "keycode_top_position";
     public static final String NOTIF_SLIDER_MIDDLE_KEY = "keycode_middle_position";
@@ -48,9 +51,27 @@ public class Constants {
     public static final int KEY_VALUE_VIBRATE = 3;
     public static final int KEY_VALUE_NORMAL = 4;
 
+    // Screen off Gesture actions
+    static final int ACTION_FLASHLIGHT = 1;
+    static final int ACTION_CAMERA = 2;
+    static final int ACTION_BROWSER = 3;
+    static final int ACTION_DIALER = 4;
+    static final int ACTION_EMAIL = 5;
+    static final int ACTION_MESSAGES = 6;
+    static final int ACTION_PLAY_PAUSE_MUSIC = 7;
+    static final int ACTION_PREVIOUS_TRACK = 8;
+    static final int ACTION_NEXT_TRACK = 9;
+    static final int ACTION_VOLUME_DOWN = 10;
+    static final int ACTION_VOLUME_UP = 11;
+
     public static final Map<String, String> sStringKeyPreferenceMap = new HashMap<>();
     public static final Map<Integer, String> sKeyMap = new HashMap<>();
     public static final Map<String, Integer> sKeyDefaultMap = new HashMap<>();
+
+    // Broadcast extra: keycode mapping (int[]: key = gesture ID, value = keycode)
+    static final String UPDATE_EXTRA_KEYCODE_MAPPING = "keycode_mappings";
+    // Broadcast extra: assigned actions (int[]: key = gesture ID, value = action)
+    static final String UPDATE_EXTRA_ACTION_MAPPING = "action_mappings";
 
     static {
         sStringKeyPreferenceMap.put(NOTIF_SLIDER_TOP_KEY, NOTIF_SLIDER_TOP_PREF);

DeviceSettings/src/com/aosip/device/DeviceSettings/Startup.java
@@ -25,7 +25,11 @@ import android.provider.Settings;
 import android.text.TextUtils;
 import androidx.preference.PreferenceManager;
 
+import com.aosip.device.DeviceSettings.TouchscreenGestureSettings;
+
 public class Startup extends BroadcastReceiver {
+
+    private static final String ONE_TIME_TUNABLE_RESTORE = "hardware_tunable_restored";
 
     @Override
     public void onReceive(final Context context, final Intent bootintent) {
@@ -33,6 +37,7 @@ public class Startup extends BroadcastReceiver {
         VibratorStrengthPreference.restore(context);
 
         boolean enabled = false;
+        TouchscreenGestureSettings.MainSettingsFragment.restoreTouchscreenGestureStates(context);
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
         enabled = sharedPrefs.getBoolean(DeviceSettings.KEY_SRGB_SWITCH, false);
         restore(SRGBModeSwitch.getFile(), enabled);
@@ -62,5 +67,15 @@ public class Startup extends BroadcastReceiver {
             return;
         }
         Utils.writeValue(file, value);
+    }
+
+    private boolean hasRestoredTunable(Context context) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        return preferences.getBoolean(ONE_TIME_TUNABLE_RESTORE, false);
+    }
+
+    private void setRestoredTunable(Context context) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        preferences.edit().putBoolean(ONE_TIME_TUNABLE_RESTORE, true).apply();
     }
 }

DeviceSettings/src/com/aosip/device/DeviceSettings/Utils.java
@@ -17,6 +17,9 @@
 */
 package com.aosip.device.DeviceSettings;
 
+import android.content.res.Resources;
+import android.util.Log;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -25,6 +28,8 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 
 public class Utils {
+
+    private static final String TAG = Utils.class.getSimpleName();
 
     /**
      * Write a string value to the specified file.
@@ -100,5 +105,25 @@ public class Utils {
             return fileValue;
         }
         return defValue;
+    }
+
+    public static String getLocalizedString(final Resources res,
+                                            final String stringName,
+                                            final String stringFormat) {
+        final String name = stringName.toLowerCase().replace(" ", "_");
+        final String nameRes = String.format(stringFormat, name);
+        return getStringForResourceName(res, nameRes, stringName);
+    }
+
+    public static String getStringForResourceName(final Resources res,
+                                                  final String resourceName,
+                                                  final String defaultValue) {
+        final int resId = res.getIdentifier(resourceName, "string", "com.aosip.device.DeviceSettings");
+        if (resId <= 0) {
+            Log.e(TAG, "No resource found for " + resourceName);
+            return defaultValue;
+        } else {
+            return res.getString(resId);
+        }
     }
 }

DerpFest-Ten/eas_device_oneplus_msm8998-common

DeviceSettings/src/com/derp/device/DeviceSettings/Constants.java
@@ -31,6 +31,9 @@ import android.media.AudioManager;
 
 public class Constants {
 
+    // Broadcast action for settings update
+    static final String UPDATE_PREFS_ACTION = "com.derp.device.DeviceSettings.UPDATE_SETTINGS";
+
     // Preference keys
     public static final String NOTIF_SLIDER_TOP_KEY = "keycode_top_position";
     public static final String NOTIF_SLIDER_MIDDLE_KEY = "keycode_middle_position";
@@ -48,9 +51,27 @@ public class Constants {
     public static final int KEY_VALUE_VIBRATE = 3;
     public static final int KEY_VALUE_NORMAL = 4;
 
+    // Screen off Gesture actions
+    static final int ACTION_FLASHLIGHT = 1;
+    static final int ACTION_CAMERA = 2;
+    static final int ACTION_BROWSER = 3;
+    static final int ACTION_DIALER = 4;
+    static final int ACTION_EMAIL = 5;
+    static final int ACTION_MESSAGES = 6;
+    static final int ACTION_PLAY_PAUSE_MUSIC = 7;
+    static final int ACTION_PREVIOUS_TRACK = 8;
+    static final int ACTION_NEXT_TRACK = 9;
+    static final int ACTION_VOLUME_DOWN = 10;
+    static final int ACTION_VOLUME_UP = 11;
+
     public static final Map<String, String> sStringKeyPreferenceMap = new HashMap<>();
     public static final Map<Integer, String> sKeyMap = new HashMap<>();
     public static final Map<String, Integer> sKeyDefaultMap = new HashMap<>();
+
+    // Broadcast extra: keycode mapping (int[]: key = gesture ID, value = keycode)
+    static final String UPDATE_EXTRA_KEYCODE_MAPPING = "keycode_mappings";
+    // Broadcast extra: assigned actions (int[]: key = gesture ID, value = action)
+    static final String UPDATE_EXTRA_ACTION_MAPPING = "action_mappings";
 
     static {
         sStringKeyPreferenceMap.put(NOTIF_SLIDER_TOP_KEY, NOTIF_SLIDER_TOP_PREF);

DeviceSettings/src/com/derp/device/DeviceSettings/Startup.java
@@ -25,7 +25,11 @@ import android.provider.Settings;
 import android.text.TextUtils;
 import androidx.preference.PreferenceManager;
 
+import com.derp.device.DeviceSettings.TouchscreenGestureSettings;
+
 public class Startup extends BroadcastReceiver {
+
+    private static final String ONE_TIME_TUNABLE_RESTORE = "hardware_tunable_restored";
 
     @Override
     public void onReceive(final Context context, final Intent bootintent) {
@@ -33,6 +37,7 @@ public class Startup extends BroadcastReceiver {
         VibratorStrengthPreference.restore(context);
 
         boolean enabled = false;
+        TouchscreenGestureSettings.MainSettingsFragment.restoreTouchscreenGestureStates(context);
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
         enabled = sharedPrefs.getBoolean(DeviceSettings.KEY_SRGB_SWITCH, false);
         restore(SRGBModeSwitch.getFile(), enabled);
@@ -62,5 +67,15 @@ public class Startup extends BroadcastReceiver {
             return;
         }
         Utils.writeValue(file, value);
+    }
+
+    private boolean hasRestoredTunable(Context context) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        return preferences.getBoolean(ONE_TIME_TUNABLE_RESTORE, false);
+    }
+
+    private void setRestoredTunable(Context context) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        preferences.edit().putBoolean(ONE_TIME_TUNABLE_RESTORE, true).apply();
     }
 }

DeviceSettings/src/com/derp/device/DeviceSettings/Utils.java
@@ -17,6 +17,9 @@
 */
 package com.derp.device.DeviceSettings;
 
+import android.content.res.Resources;
+import android.util.Log;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -25,6 +28,8 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 
 public class Utils {
+
+    private static final String TAG = Utils.class.getSimpleName();
 
     /**
      * Write a string value to the specified file.
@@ -100,5 +105,25 @@ public class Utils {
             return fileValue;
         }
         return defValue;
+    }
+
+    public static String getLocalizedString(final Resources res,
+                                            final String stringName,
+                                            final String stringFormat) {
+        final String name = stringName.toLowerCase().replace(" ", "_");
+        final String nameRes = String.format(stringFormat, name);
+        return getStringForResourceName(res, nameRes, stringName);
+    }
+
+    public static String getStringForResourceName(final Resources res,
+                                                  final String resourceName,
+                                                  final String defaultValue) {
+        final int resId = res.getIdentifier(resourceName, "string", "com.derp.device.DeviceSettings");
+        if (resId <= 0) {
+            Log.e(TAG, "No resource found for " + resourceName);
+            return defaultValue;
+        } else {
+            return res.getString(resId);
+        }
     }
 }